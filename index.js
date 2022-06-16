a0a5bf54a33fd65997bbab14894e2f6ab60bf116
purity-wangu <purity.kariuki@student.moringaschool.com>
2022-06-16 10:10:10 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(){
+    const cats = ["Milo", "Otis", "Garfield"];
+    return [...cats, "Broom"];
+}
+
+function prependCat(){
+    return ["Arnold", ...cats];
+}
+
+function removeLastCat(){
+    const cats = ["Milo", "Otis", "Garfield"];
+    return cats.slice(0, cats.length -1);
+}
+
+function removeFirstCat(){
+    const cats = ["Milo", "Otis", "Garfield"];
+    return cats.slice(1);
+}
